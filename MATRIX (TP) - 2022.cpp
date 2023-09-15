9f75958d52306b7bdba1e34adfcfe896e488204a
daniel <danielrodrigues878@hotmail.com>
2023-09-15 11:48:50 -0300

    Matrix - Trabalho Prático

---
+++ MATRIX (TP) - 2022.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const int N = 20; // tamanho da matriz
+const int num_iterations = 100; // número de iterações
+const int num_races = 3; // número de raças
+
+
+
+// função para gerar valores aleatórios
+int rand_int(int min, int max) {
+  return min + (std::rand() % (max - min + 1));
+}
+
+// função para imprimir a matriz
+void print_matrix(int matrix[N][N]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      std::cout << matrix[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+int main() {
+  // inicializar gerador de números aleatórios
+  std::srand(std::time(0));
+
+  // criar matriz
+  int matrix[N][N];
+  
+  // preencher a matriz com valores aleatórios                   
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      matrix[i][j] = rand_int(1, num_races-1 );
+    }
+  }
+
+// Insere 10% dos valores como 0
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (rand() % 6 == 0)
+        {
+            matrix[i][j] = 0;
+        }
+    }
+}
+  // imprimir matriz inicial
+  std::cout << "Matriz inicial:\n";
+  print_matrix(matrix);
+
+  // loop pelo número de iterações
+  for (int it = 0; it < num_iterations; it++) {
+    // loop pelos elementos da matriz
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        // calcular quantos vizinhos são iguais ao elemento atual
+        int num_neighbors = 0;
+        if (i > 0 && matrix[i - 1][j] == matrix[i][j]) num_neighbors++;
+        if (i < N - 1 && matrix[i + 1][j] == matrix[i][j]) num_neighbors++;
+        if (j > 0 && matrix[i][j - 1] == matrix[i][j]) num_neighbors++;
+        if (j < N - 1 && matrix[i][j + 1] == matrix[i][j]) num_neighbors++;
+
+        if (i > 0 && matrix[i - 1][j] == matrix[i][j]) num_neighbors++;
+        if (i < N - 1 && matrix[i + 1][j] == matrix[i][j]) num_neighbors++;
+        if (i > 0 && matrix[i - 1][j] == matrix[i][j]) num_neighbors++;
+        if (i < N - 1 && matrix[i + 1][j] == matrix[i][j]) num_neighbors++;
+
+
+        if (i > 0 && matrix[i - 1][j] == matrix[i][j]) num_neighbors++;
+        if (i < N - 1 && matrix[i + 1][j] == matrix[i][j]) num_neighbors++;
+
+        // se o elemento tiver menos de 5 vizinhos iguais, escolher novo valor aleatório para ele
+        if (num_neighbors < 5) matrix[i][j] = rand_int( 0, num_races-1 );
+      }
+    }
+  }
+
+
+  // imprimir matriz final
+  std::cout << "\nMatriz final:\n";
+  print_matrix(matrix);
+
+std::cout << std::endl;
+    
+  system("PAUSE");
+ return EXIT_SUCCESS;
+}
